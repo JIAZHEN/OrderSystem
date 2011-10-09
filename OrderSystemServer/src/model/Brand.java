2977624ba391d085309da00b987786980e501e84
Ting Dao <dtcs519@gmail.com>
2011-10-09 11:38:46 +0100

    Signed-off-by: Ting Dao <dtcs519@gmail.com>

---
+++ OrderSystemServer/src/model/Brand.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Jacob
+ */
+public class Brand implements Serializable{
+    private int brandId;
+    private String brandEName;
+    private String brandCName;
+
+    /**
+     * @return the brandId
+     */
+    public int getBrandId() {
+        return brandId;
+    }
+
+    /**
+     * @param brandId the brandId to set
+     */
+    public void setBrandId(int brandId) {
+        this.brandId = brandId;
+    }
+
+    /**
+     * @return the brandEName
+     */
+    public String getBrandEName() {
+        return brandEName;
+    }
+
+    /**
+     * @param brandEName the brandEName to set
+     */
+    public void setBrandEName(String brandEName) {
+        this.brandEName = brandEName;
+    }
+
+    /**
+     * @return the brandCName
+     */
+    public String getBrandCName() {
+        return brandCName;
+    }
+
+    /**
+     * @param brandCName the brandCName to set
+     */
+    public void setBrandCName(String brandCName) {
+        this.brandCName = brandCName;
+    }
+}
---
+++ OrderSystemServer/src/model/Category.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Jacob
+ */
+public class Category  implements Serializable{
+    private int cateId;
+    private String cateEName;
+    private String cateCName;
+
+    /**
+     * @return the cateId
+     */
+    public int getCateId() {
+        return cateId;
+    }
+
+    /**
+     * @param cateId the cateId to set
+     */
+    public void setCateId(int cateId) {
+        this.cateId = cateId;
+    }
+
+    /**
+     * @return the cateEName
+     */
+    public String getCateEName() {
+        return cateEName;
+    }
+
+    /**
+     * @param cateEName the cateEName to set
+     */
+    public void setCateEName(String cateEName) {
+        this.cateEName = cateEName;
+    }
+
+    /**
+     * @return the cateCName
+     */
+    public String getCateCName() {
+        return cateCName;
+    }
+
+    /**
+     * @param cateCName the cateCName to set
+     */
+    public void setCateCName(String cateCName) {
+        this.cateCName = cateCName;
+    }
+    
+}
